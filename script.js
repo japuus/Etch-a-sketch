94a8935c7c86b8f85dc3c25785082f794f84ca0c
japuus <50738467+japuus@users.noreply.github.com>
2024-11-25 20:29:16 +0200

    Add loop which creates divs

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-// script for Etch-a-sketch project
+const container = document.querySelector("#container");
+
+for (let i = 0; i < 64; i++) { 
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    cell.textContent = "cell text";
+    container.appendChild(cell); 
+    }
