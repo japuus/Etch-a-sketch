10a628dc8da636a2c46191e2232b827a9bd6d837
japuus <50738467+japuus@users.noreply.github.com>
2025-02-12 17:02:12 +0200

    Add coloring function and Clear All function

--- script.js
+++ script.js
@@ -1,8 +1,53 @@
 const container = document.querySelector("#container");
+let color = ["#3C9EE7", "#E7993C", "#E73C99", "#3CE746", "#E7993C"];
+let blue = "#3C9EE7";
+let white = "#FFFFFF";
+let mouseDown = false;
+//let opasiteetti = 0.1;
 
-for (let i = 0; i < 64; i++) { 
+document.body.addEventListener("mousedown", () => {
+    mouseDown = true;
+});
+
+document.body.addEventListener("mouseup", () => {
+    mouseDown = false;
+});
+
+for (let i = 0; i < 256; i++) { 
     const cell = document.createElement("div");
     cell.classList.add("cell");
-    cell.textContent = "cell text";
-    container.appendChild(cell); 
-    }
+    cell.style.border = "1px solid rgba(0, 0, 0, 1)";
+    cell.dataset.opacity = 0.1;
+    container.appendChild(cell);
+    cell.addEventListener("mouseover", () => {
+        if (mouseDown) {
+            cell.style.backgroundColor = blue;
+            let currentOpacity = parseFloat(cell.dataset.opacity);
+            currentOpacity += 0.1;
+            cell.style.opacity = currentOpacity;
+            cell.dataset.opacity = currentOpacity;
+            // cell.style.background = color[(Math.floor(Math.random() * color.length))];
+        }
+    });
+
+ 
+    cell.addEventListener("dragstart", (event) => {
+      event.preventDefault();
+    });
+    cell.addEventListener("dragend", (event) => {
+      event.preventDefault(); mouseDown = false;
+    });
+
+}
+
+const clearAllButton = document.querySelector(".clearAllButton");
+const allCells = document.querySelectorAll(".cell");
+
+clearAllButton.addEventListener("click", () => {
+allCells.forEach(cell => {
+    cell.style.background = white;
+    cell.style.opacity = 1;
+    opacity = 0.1;
+    cell.dataset.opacity = 0.1;
+});
+});
